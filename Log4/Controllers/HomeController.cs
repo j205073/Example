0ff9331607106337d00e097eac1bccddade0bfc5
j205073 <j205073@gmail.com>
2017-07-27 17:04:02 +0800

    update

---
+++ Log4/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Log4.Controllers
+{
+    public class HomeController : Controller
+    {
+        ILog log = log4net.LogManager.GetLogger(typeof(HomeController));
+
+        public ActionResult Index()
+        {
+            log.Debug("Debug message");
+            log.Warn("Warn message");
+            log.Error("Error message");
+            log.Fatal("Fatal message");
+            ViewBag.Title = "Home Page";
+            return View();
+        }
+	}
+}
---
+++ WindowsService1/Service1.cs
@@ -0,0 +1,58 @@
+﻿using System.Diagnostics;
+using System.ServiceProcess;
+using System.Timers;
+
+namespace WindowsService1
+{
+    public partial class Service1 : ServiceBase
+    {
+        private Timer MyTimer;
+
+        public Service1()
+        {
+            InitializeComponent();
+            this.AutoLog = false;
+
+            if (!System.Diagnostics.EventLog.SourceExists("MySource"))
+            {
+                System.Diagnostics.EventLog.CreateEventSource(
+
+                    "MySource", "MyLog");
+            }
+
+            eventLog1.Source = "MySource";
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            eventLog1.WriteEntry("Start Timer.");
+
+            MyTimer = new Timer();
+
+            MyTimer.Elapsed += new ElapsedEventHandler(MyTimer_Elapsed);
+
+            MyTimer.Interval = 10 * 1000;
+
+            MyTimer.Start();
+        }
+
+        private void MyTimer_Elapsed(object sender, ElapsedEventArgs e)
+        {
+            //寫入資訊在這
+            eventLog1.WriteEntry("Timer Ticked.");
+        }
+
+        private void eventLog1_EntryWritten(object sender, EntryWrittenEventArgs e)
+        {
+        }
+
+        protected override void OnStop()
+        {
+            eventLog1.WriteEntry("Stop Timer.");
+
+            MyTimer.Stop();
+
+            MyTimer = null;
+        }
+    }
+}

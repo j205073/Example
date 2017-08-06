179ce7450e41e1337b73b50c6105d80b41c02f9f
JACKY <j205073@gmail.com>
2017-08-06 11:48:04 +0800

    u

---
+++ Reflection/Form1.cs
@@ -0,0 +1,73 @@
+﻿using Reflection.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Reflection
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Model1 model = new Model1() { ID = 1, Name = "jacky" };
+            PrintData(model);
+        }
+        private void PrintData(dynamic objVo)
+        {
+            Type objType = objVo.GetType();
+            PropertyInfo[] propInfos = objType.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+            foreach (var propInfo in propInfos)
+            {
+                var pName = propInfo.Name;
+                var pValue = propInfo.GetValue(objVo, null);
+
+
+
+
+                bool readable = propInfo.CanRead;
+                bool writable = propInfo.CanWrite;
+
+                if (readable)
+                {
+                    MethodInfo getAccessor = propInfo.GetMethod;
+                    Console.WriteLine("   Visibility:    {0}",
+                                      GetVisibility(getAccessor));
+                }
+                if (writable)
+                {
+                    MethodInfo setAccessor = propInfo.SetMethod;
+                    Console.WriteLine("   Visibility:    {0}",
+                                      GetVisibility(setAccessor));
+                }
+
+            }
+
+        }
+        public static String GetVisibility(MethodInfo accessor)
+        {
+            if (accessor.IsPublic)
+                return "Public";
+            else if (accessor.IsPrivate)
+                return "Private";
+            else if (accessor.IsFamily)
+                return "Protected";
+            else if (accessor.IsAssembly)
+                return "Internal/Friend";
+            else
+                return "Protected Internal/Friend";
+        }
+    }
+}
---
+++ Reflection/Models/Model1.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Reflection.Models
+{
+    public class Model1
+    {
+        int m_id = 0;
+        string m_name = "";
+        public int ID { get { return this.m_id; } set { this.m_id = value; } }
+        public string Name { get { return this.m_name; } set { this.m_name = value; } }
+
+    }
+}
